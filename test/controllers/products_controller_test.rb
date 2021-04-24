30fbff9a72542a0bdafe3343e24eab1888083000
Sonia <glushchak02@gmail.com>
2021-04-24 18:08:41 +0300

    create cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -40,7 +40,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
-      delete product_url(@product)
+    delete product_url(@product)
     end
     
     assert_redirected_to products_url
@@ -52,5 +52,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select   'h2', text: product.title
+  end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+    delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
   end
 end
