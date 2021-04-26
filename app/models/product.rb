14aa631c681fde14e7ad99d8f440696a0a8544ca
Sonia <glushchak02@gmail.com>
2021-04-26 14:36:59 +0300

    Creating a Smarter Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,8 +15,8 @@ class Product < ApplicationRecord
 
     def ensure_not_referenced_by_any_line_item
       unless line_items.empty?
-      errors.add(:base, 'Line Items present')
-      throw :abort
+        errors.add(:base, 'Line Items present')
+        throw :abort
       end
     end
 end
