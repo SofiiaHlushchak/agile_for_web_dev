2f80941c7c87b5d519c490b913cb8e5ec08023d5
Sonia <glushchak02@gmail.com>
2021-05-10 22:35:47 +0300

    add_internationalization

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bcrypt', '~> 3.1.7'
 gem 'pry-rails'
 gem 'faker'
-
+gem 'i18n-js'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,12 @@ Rails.application.routes.draw do
   resources :products do
     get :who_bought, on: :member
   end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
